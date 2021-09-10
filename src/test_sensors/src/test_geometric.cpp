b3224e0e61574cea150f1429674638642a640bca
Davt2000 <davtart2000@yandex.ru>
2021-09-10 12:32:10 +0300

    Implemented random sensor data publisher

--- src/test_sensors/src/test_geometric.cpp
+++ src/test_sensors/src/test_geometric.cpp
@@ -1,10 +1,36 @@
 #include <cstdio>
+#include <chrono>
+#include <memory>
+#include <string>
+#include <cstdlib>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+class SensorPublisher : public rclcpp::Node {
+  public:
+    SensorPublisher(): Node("test_sensor_publisher"){
+        publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+      timer_ = this->create_wall_timer(
+          500ms, std::bind(&SensorPublisher::timer_callback, this));
+    }
+  private:
+    void timer_callback(){
+        auto message = std_msgs::msg::String();
+        message.data = std::to_string(rand()%101-50);
+        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+        publisher_->publish(message);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+};
 
 int main(int argc, char ** argv)
 {
-  (void) argc;
-  (void) argv;
-
-  printf("hello world test_sensors package\n");
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<SensorPublisher>());
+  rclcpp::shutdown();
   return 0;
 }
